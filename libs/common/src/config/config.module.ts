1e60ba761bc5a14f3d340a1a98c306b9072bf42d
abolfazl jazak <jazakabolfazl@gmail.com>
2025-09-01 16:47:50 +0330

    feat(config): Enhance ConfigModule with global scope and Joi validation for MONGODB_URI; update DatabaseModule to import ConfigModule

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+
 @Module({
   imports: [
     NestConfigModule.forRoot({
-      validationSchema: Joi.string().required(),
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      })
     }),
   ],
 })
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
+      imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         uri: configService.get('MONGODB_URI'),
       }),
