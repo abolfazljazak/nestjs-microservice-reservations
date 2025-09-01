046c6bebd266f773a694f92e97a4cc6654ac92ff
abolfazl jazak <jazakabolfazl@gmail.com>
2025-09-01 21:35:53 +0330

    feat(reservations): Integrate Pino logger and validation pipe in ReservationsModule; update package dependencies for logging and validation

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -7,6 +7,7 @@ import {
   ReservationDocument,
   ReservationSchema,
 } from './models/reservation.schema';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
@@ -17,6 +18,16 @@ import {
         schema: ReservationSchema,
       },
     ]),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          options: {
+            singleLine: true,
+          },
+          target: 'pino-pretty',
+        },
+      },
+    }),
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
